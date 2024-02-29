1e705952f25763f9f2f71b85664f1cbe4d37142f
Victor Payno <vpayno@users.noreply.github.com>
2024-02-28 20:35:58 -0800

    fix: make install-git-aliases.sh verbose

--- install-git-aliases.sh
+++ install-git-aliases.sh
@@ -20,10 +20,32 @@ if [[ ! -d ${HOME}/.git-helpers ]]; then
 	printf "\n"
 fi
 
+printf "Installing git-helpers git aliases...\n\n"
+
 git config --global alias.bc '!'"${PWD}"'/git-clean-up-branches-helper --headless'
+git config --global alias.bc
+printf "\n"
+
 git config --global alias.hf '!'"${PWD}"'/git-fixup-helper'
+git config --global alias.hf
+printf "\n"
+
 git config --global alias.hc '!'"${PWD}"'/git-commit-helper'
+git config --global alias.hc
+printf "\n"
+
 git config --global alias.hbd '!'"${PWD}"'/git-delete-branch-helper'
+git config --global alias.hbd
+printf "\n"
+
 git config --global alias.hnb '!'"${PWD}"'/git-new-branch-helper'
+git config --global alias.hnb
+printf "\n"
+
 git config --global alias.fetchcr '!'"${PWD}"'/git-fetch-cr-helper'
+git config --global alias.fetchcr
+printf "\n"
+
 git config --global alias.master2main '!'"${PWD}"'/git-master-to-main-migration-helper'
+git config --global alias.master2main
+printf "\n"
